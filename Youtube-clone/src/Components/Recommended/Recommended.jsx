7c3978f4dfd7864584ca5207108950249ab90178
Itssagarcode <sagarmaithil70@gmail.com>
2024-02-18 01:34:37 +0530

    Youtube clone using raect

---
+++ Youtube-clone/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import './Recommended.css'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor  consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolo consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor sconsectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem ipsum dolor consectetur.</h4>
+                <p>Chai Aur Code</p>
+                <p>40k</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Recommended
