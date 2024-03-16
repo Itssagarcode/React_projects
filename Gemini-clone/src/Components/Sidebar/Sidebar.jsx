1ca30500c8323e6667176dbfcfbb39484a85d7a9
Itssagarcode <sagarmaithil70@gmail.com>
2024-03-16 15:51:36 +0530

    home

--- Gemini-clone/src/Components/Sidebar/Sidebar.jsx
+++ Gemini-clone/src/Components/Sidebar/Sidebar.jsx
@@ -38,15 +38,15 @@ const Sidebar = () => {
       </div>
       <div className="bottom">
             <div className="bottom-item recent-entry">
-                <img src={assets.question_icon} alt="" />
+                <img className='icon' src={assets.question_icon} alt="" />
                 {extended? <p>Help</p>: null}
             </div>
             <div className="bottom-item recent-entry">
-                <img src={assets.history_icon} alt="" />
+                <img className='icon' src={assets.history_icon} alt="" />
                 {extended? <p>Activity</p>: null}
             </div>
             <div  className="bottom-item recent-entry">
-                <img src={assets.setting_icon} alt="" />
+                <img className='icon' src={assets.setting_icon} alt="" />
                 {extended? <p>Settings</p>: null}
             </div>
       </div>
